ff110085b8819109a9c518f6c73bbc45e3cc9cc9
L-2022 <bilyakvasya@ukr.net>
2023-01-13 05:21:43 +0200

    add Reviews and user name for server

---
+++ server/controllers/userController.js
@@ -0,0 +1,54 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
+class UserController {
+    async registration(req, res, next) {
+        const {email, password, role, userName} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Некоректний email або password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('Користувач з таким email вже зареєстрований!'))
+        }
+        const hashPassword = await bcrypt.hash(password, 7)
+
+        const user = await User.create({email, role, password: hashPassword, userName})
+        const basket = await Basket.create({userId: user.id})
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
+
+    }
+
+
+    async login(req, res, next) {
+        const {email, password} = req.body
+        const user = await User.findOne({where: {email}})
+        if (!user) {
+            return next(ApiError.internal('Користувач відсутній'))
+        }
+        let comparePassword = bcrypt.compareSync(password, user.password)
+        if (!comparePassword) {
+            return next(ApiError.internal('Помилковий пароль'))
+        }
+        const token = generateJwt(user.id, user.email, user.role)
+        return res.json({token})
+    }
+
+    async check(req, res, next) {
+        const token = generateJwt(req.user.id, req.user.email, req.user.role)
+        return res.json({token})
+    }
+}
+
+module.exports = new UserController()
---
+++ server/routes/deviceRouter.js
@@ -0,0 +1,9 @@
+const Router = require('express')
+const router = new Router()
+const deviceController = require('../controllers/deviceController')
+
+router.post('/', deviceController.create)
+router.get('/', deviceController.getAll)
+router.get('/:id', deviceController.getOne)
+router.post('/t', deviceController.createReview)
+module.exports = router
